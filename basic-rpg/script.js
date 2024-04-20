902023aebc97fb0df5d55e234af2884b1abc843f
Drake224025 <sasithamare@gmail.com>
2024-04-20 22:54:06 +0530

    Update file

--- basic-rpg/script.js
+++ basic-rpg/script.js
@@ -62,6 +62,12 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters.",
   },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster.",
+  },
 ];
 
 // initialize buttons
@@ -134,12 +140,35 @@ function sellWeapon() {
   }
 }
 
-function fightSlime() {}
-
-function fightBeast() {}
-
-function fightDragon() {
-  console.log("Fighting dragon.");
+function fightSlime() {
+  fighting = 0;
+  goFight();
 }
 
-function goFight() {}
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
+}
+
+function dodge() {}
