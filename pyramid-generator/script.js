a92ccebf5bb7256240b90b10da6aa7e32b399928
Drake224025 <sasithamare@gmail.com>
2024-05-28 20:15:11 +0530

    Step 23

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -1,6 +1,8 @@
 let character = "Hello";
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
-rows[rows.length - 1] = 10;
+rows.push("freeCodeCamp");
+
+let popped = rows.pop();
+console.log(popped);
 console.log(rows);
