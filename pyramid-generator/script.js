4ef06b8558d599da7157c204bd7d494f384ebfed
Drake224025 <sasithamare@gmail.com>
2024-06-17 20:44:38 +0530

    Step 60

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -4,9 +4,10 @@ const rows = [];
 
 function padRow(name) {
   const test = "Testing";
+  console.log("This works!");
   return test;
+  console.log("This works!");
 }
-
 const call = padRow("CamperChan");
 console.log(call);
 
