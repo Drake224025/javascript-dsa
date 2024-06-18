58a17843309b02173386d05db6d7572dc749c1ed
Drake224025 <sasithamare@gmail.com>
2024-06-18 21:19:24 +0530

    Step 64

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -2,14 +2,7 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
-  const test = "Testing";
-  console.log("This works!");
-  return test;
-  console.log("This works!");
-}
-const call = padRow("CamperChan");
-console.log(call);
+function padRow(rowNumber, rowCount) {}
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1));
