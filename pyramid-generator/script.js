b8046aa6defd6a7c75fd3e91c1eacae005178b3b
Drake224025 <sasithamare@gmail.com>
2024-06-19 21:53:49 +0530

    Step 65

--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -2,7 +2,9 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(rowNumber, rowCount) {}
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1));
