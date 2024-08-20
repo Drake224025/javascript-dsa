c3d092735471618761e6b577c43e81d5d3b6742f
Drake224025 <sasithamare@gmail.com>
2024-08-20 20:56:12 +0530

    Step 16

--- music-player/index.js
+++ music-player/index.js
@@ -84,7 +84,6 @@ let userData = {
   currentSong: null,
   songCurrentTime: 0,
 };
-
 const printGreeting = () => {
   console.log("Hello there!");
 };
@@ -96,7 +95,7 @@ const printMessage = (org) => {
 };
 
 printMessage("freeCodeCamp");
-const addTwoNumbers = (num1, num2) => {
-  return num1 + num2;
-};
+
+const addTwoNumbers = (num1, num2) => num1 + num2;
+
 console.log(addTwoNumbers(3, 4));
