6ae79334bfb8e2695dde672ba5df523e39982b17
Drake224025 <sasithamare@gmail.com>
2024-10-04 22:03:37 +0530

    Step 60

--- music-player/index.js
+++ music-player/index.js
@@ -123,6 +123,9 @@ const playPreviousSong = () => {
   if (userData?.currentSong === null) return;
   else {
     const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
   }
 };
 
